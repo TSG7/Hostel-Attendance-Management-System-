f962bd591ec55a0a8cdae1b6f3ffc0cb8d17a6b7
VIKAS <vikasdharavath04@gmail.com>
2025-07-15 08:14:52 +0530

    Update Profile.jsx

--- hostel-attendance-management/components/Profile.jsx
+++ hostel-attendance-management/components/Profile.jsx
@@ -16,25 +16,30 @@ export default function Profile() {
   }
 
   return (
-      <div className="w-full min-h-screen flex items-center justify-center">
-  <div className="bg-[#2e2e2e] border border-blue-500 rounded-xl p-8 shadow-lg flex flex-col items-center space-y-4">
+   <div className="content">
+  {/* Title User Profile */}
+ <h1 className="blue_gradient text-5xl font-bold">User Profile</h1>
 
-    {/* Profile Image */}
-    <img
-      src={session.user.image}
-      alt="Profile"
-      className="w-28 h-28 rounded-full object-cover border-2 border-blue-400"
-      draggable={false}
-    />
+  {/* Profile Card */}
+  <div className="flex justify-center mt-6">
+    <div className="bg-[#2e2e2e] border border-blue-500 rounded-xl p-8 shadow-lg flex flex-col items-center space-y-4">
 
-    {/* Profile Details */}
-    <div className="text-center text-white space-y-2">
-      <h2 className="text-xl font-semibold text-blue-400">{session.user.name}</h2>
-      <p><span className="text-gray-300 font-medium">Course:</span> B.Tech</p>
-      <p><span className="text-gray-300 font-medium">Email ID:</span> {session.user.email}</p>
-      <p><span className="text-gray-300 font-medium">Hostel:</span> C V Raman</p>
+      {/* Profile Image */}
+      <img
+        src={session.user.image}
+        alt="Profile"
+        className="w-28 h-28 rounded-full object-cover border-2 border-blue-400"
+        draggable={false}
+      />
+
+      {/* Profile Details */}
+      <div className="text-center text-white space-y-2">
+        <h2 className="text-xl font-semibold text-blue-400">{session.user.name}</h2>
+        <p><span className="text-gray-300 font-medium">Course:</span> B.Tech</p>
+        <p><span className="text-gray-300 font-medium">Email ID:</span> {session.user.email}</p>
+        <p><span className="text-gray-300 font-medium">Hostel:</span> C V Raman</p>
+      </div>
     </div>
-
   </div>
 </div>
 
